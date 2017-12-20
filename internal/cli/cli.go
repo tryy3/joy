ba0325ef80d6f76c17e29785f7ce28e986c53a96
Tryy3 <Dennistryy3@gmail.com>
2017-12-20 15:31:46 +0100

    Update cli.go

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,9 +1,15 @@
 package cli
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"github.com/andrew-d/go-termutil"
+	"github.com/matthewmueller/joy/internal/paths"
+	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"runtime"
 	"time"
 
@@ -59,12 +65,39 @@ func Run(ctx context.Context, ver string) (err error) {
 		}
 	}
 
-	// special case: joy ./main & ./main.go
-	if len(os.Args[1:]) == 1 {
-		if _, err := os.Stat(os.Args[1]); !os.IsNotExist(err) {
+	// special case: joy ./main & ./main.go or when someone pipes data to
+	stat, _ := os.Stdin.Stat()
+	if len(os.Args[1:]) == 1 || (stat.Mode()&os.ModeCharDevice == 0) {
+		file := ""
+		if len(os.Args[1:]) == 1 {
+			file = os.Args[1]
+		} else {
+			defer removeTempFile()
+			p, err := paths.Joy()
+			if err != nil {
+				return errors.Wrapf(err, "error getting joy's root path")
+			}
+
+			goSrc, err := readStreamInput()
+			if err != nil {
+				return errors.Wrapf(err, "error reading stream input")
+			}
+
+			tmpDir := filepath.Join(p, "tmp")
+			if err := os.Mkdir(tmpDir, os.ModePerm); err != nil {
+				return errors.Wrapf(err, "error creating temp folder")
+			}
+
+			file = filepath.Join(tmpDir, "main.go")
+			if err := ioutil.WriteFile(file, []byte(goSrc), os.ModePerm); err != nil {
+				return errors.Wrapf(err, "error creating temp file")
+			}
+		}
+
+		if _, err := os.Stat(file); !os.IsNotExist(err) {
 			files, err := apibuild.Build(&apibuild.Config{
 				Context:  ctx,
-				Packages: []string{os.Args[1]},
+				Packages: []string{file},
 			})
 			if err != nil {
 				return errors.Wrapf(err, "error building code")
@@ -88,4 +121,39 @@ func Run(ctx context.Context, ver string) (err error) {
 	// run the command
 	_, err = cmd.Parse(os.Args[1:])
 	return err
+}
+
+func readStreamInput() (string, error) {
+	r := bufio.NewReader(os.Stdin)
+	str := ""
+	buf := make([]byte, 0, 4*1024)
+	for {
+		n, err := r.Read(buf[:cap(buf)])
+		buf = buf[:n]
+		if n == 0 {
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+		// process buf
+		str += string(buf)
+		if err != nil && err != io.EOF {
+			return "", err
+		}
+	}
+	return str, nil
+}
+
+func removeTempFile() {
+	p, err := paths.Joy()
+	if err != nil {
+		return
+	}
+
+	tmpDir := filepath.Join(p, "tmp")
+	err = os.RemoveAll(tmpDir)
 }
